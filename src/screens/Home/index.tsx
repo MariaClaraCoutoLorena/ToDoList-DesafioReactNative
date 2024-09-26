65018ab028518a2f60007519a64e7a618c383bbe
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2024-09-26 12:00:47 -0300

    add logo and html base

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,44 @@
+import { View, Text } from 'react-native';
+
+export default function Home() {
+
+    return(
+        <View>
+
+            <View>
+                <img src="../../../assets/logo.png" alt="Logo com um aivão da Rocketseat e a frase to do" />
+            </View>
+
+            <View>
+                
+
+
+            </View>
+
+
+            <View>
+
+                <View>
+                    <Text> 
+                        Criadas
+                    </Text>
+
+                    <Text>
+                        Concluídas
+                    </Text>
+
+                </View>
+
+                <Text>
+                    Você ainda não tem tarefas cadastradas
+                </Text>
+
+                <Text>
+                    Crie tarefas e organize seus itens a fazer
+                </Text>
+            </View>
+
+        </View>
+    )
+
+}
